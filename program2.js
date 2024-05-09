c70842d1d170c6a2e30f0d2814d47d50e45c3864
Aryasoniya44 <161592345+Aryasoniya44@users.noreply.github.com>
2024-05-09 13:27:50 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,60 @@
 const decodeTheRing = function (s, p) {
 
-    // write your code here
+  function isDecoderKeyValid(message, key) {
+    // If message and key have different lengths, return false
+    if (message.length !== key.length) {
+        return false;
+    }
 
-  };
+  
+    for (let i = 0; i < message.length; i++) {
+        const messageChar = message[i];
+        const keyChar = key[i];
+
+      
+        if (keyChar !== '?' && keyChar !== '*') {
+          
+            if (messageChar !== keyChar) {
+                return false;
+            }
+        } else if (keyChar === '?') {
+            
+            continue;
+        } else if (keyChar === '*') {
+            
+            let nextCharPosition = i + 1;
+            let nextKeyChar = key[nextCharPosition];
+            // Skip any consecutive stars in the key
+            while (nextKeyChar === '*') {
+                nextCharPosition++;
+                nextKeyChar = key[nextCharPosition];
+            }
+           
+            if (nextKeyChar === undefined) {
+                return true;
+            }
+           
+            const nextCharIndex = message.indexOf(nextKeyChar, i + 1);
+           
+            if (nextCharIndex === -1) {
+                return false;
+            }
+           
+            i = nextCharIndex - 1;
+        }
+    }
+
+    
+    return true;
+}
+
+// Examples:
+console.log(isDecoderKeyValid("aa", "a"));  // Output: false
+console.log(isDecoderKeyValid("aa", "*"));  // Output: true
+console.log(isDecoderKeyValid("cb", "?a"));  // Output: false
+
+
+
   
   module.exports = decodeTheRing;
+};
